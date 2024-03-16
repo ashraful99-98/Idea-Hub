a4b0fe0d06e36ace93a5526f89b752711a88fc38
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-03-17 00:10:25 +0600

    Heroes added

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -2,8 +2,28 @@ import Image from "next/image";
 
 export const Heroes = () =>{
     return(
-        <div>
-          Heroes  
+        <div className=" flex flex-col items-center justify-center">
+            <div className=" flex items-center">
+                <div className=" relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+                    <Image
+                    src="/documents.png"
+                    fill
+                    alt="documents"
+                    className=" object-contain"
+                    />
+                </div>
+                <div className=" relative h-[400px] w-[400px] hidden md:block">
+                    <Image
+                    src="/reading.png"
+                    fill
+                    alt="Reading"
+                    className=" object-contain"
+                    />
+
+                </div>
+
+            </div>
+           
         </div>
     )
 }
