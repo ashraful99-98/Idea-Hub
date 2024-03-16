c1dcf8b413d6332389986ab3a64fe7235289b3f6
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-03-17 03:17:56 +0600

    added navbar and themes switcher

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -9,7 +9,13 @@ export const Heroes = () =>{
                     src="/documents.png"
                     fill
                     alt="documents"
-                    className=" object-contain"
+                    className=" object-contain dark:block"
+                    />
+                    <Image
+                    src="/documents-dark.png"
+                    fill
+                    alt="documents"
+                    className=" object-contain hidden dark:block"
                     />
                 </div>
                 <div className=" relative h-[400px] w-[400px] hidden md:block">
@@ -17,7 +23,13 @@ export const Heroes = () =>{
                     src="/reading.png"
                     fill
                     alt="Reading"
-                    className=" object-contain"
+                    className=" object-contain dark:block"
+                    />
+                    <Image
+                    src="/reading-dark.png"
+                    fill
+                    alt="Reading"
+                    className=" object-contain hidden dark:block"
                     />
 
                 </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/themes-providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,8 +31,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressContentEditableWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        storageKey="jotion-theme-2"
+        >{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
