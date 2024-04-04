713814ae9ce10fcc44bfc949bb09662b6925e039
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-04-05 01:15:57 +0600

    search,setting&New-page added

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { cn } from "@/lib/utils";
-import { ChevronLast, ChevronLeft,MenuIcon, PlusCircle } from "lucide-react";
+import { ChevronLast, ChevronLeft,MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -132,6 +132,17 @@ export const Navigation = () => {
             </div>
             <div>
                <UserItem/>
+               <Item
+                label="Search"
+                icon={Search}
+                isSearch
+                onClick={()=>{}}
+               />
+               <Item
+                label="Settings"
+                icon={Settings}
+                onClick={()=>{}}
+               />
                <Item
                  onClick={handleCreate} 
                  label="New page" 
