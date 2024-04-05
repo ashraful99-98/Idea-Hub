ba468db32e7ade9730a9d5dc834ea88fb9e4a83a
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-04-06 00:47:12 +0600

    document-list added

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,10 +5,11 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -18,7 +19,7 @@ export const Navigation = () => {
     const navbarRef = useRef<ElementRef<"div">>(null);
     const [isResetting, setIsResetting] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(isMobile);
-    const documents = useQuery(api.documents.get);
+
     
     const create = useMutation(api.documents.create);
 
@@ -150,11 +151,7 @@ export const Navigation = () => {
                 />
             </div>
             <div className=" mt-4">
-                {documents?.map((document)=>(
-                    <p key={document._id}>
-                        {document.title}
-                    </p>
-                ))}
+               <DocumentList/>
             </div>
             <div
             onMouseDown={handleMouseDown}
