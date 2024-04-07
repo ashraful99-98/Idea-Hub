94b0ed76e0a0f66da09cbc6afb9a757e30451837
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-04-07 17:49:17 +0600

    added search-command bar

---
+++ components/ui/search-command.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+
+import {useEffect, useState} from "react";
+import {File} from "lucide-react";
+import {useQuery} from "convex/react";
+import {useRouter} from "next/navigation";
+import {useUser} from "@clerk/clerk-react";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from "@/components/ui/command";
+
+import {useSearch} from "@/hooks/use-search";
+import {api} from "@/convex/_generated/api";
+
+export const SearchCommand = () =>{
+    const {user} = useUser();
+    const  router = useRouter();
+    const documents = useQuery(api.documents.getSearch);
+    const [isMounted, setIsMounted] = useState(false);
+
+
+    const toggle = useSearch((store)=>store.toggle);
+    const isOpen = useSearch((store)=>store.isOpen);
+    const onClose = useSearch((store)=>store.onClose);
+
+    useEffect(()=>{
+        setIsMounted(true);
+    },[]);
+
+    if(!isMounted){
+        return null;
+    }
+}
+
