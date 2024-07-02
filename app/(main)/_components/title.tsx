a48e89df043b76f78c4fbd4e7497c490bc9e1b1d
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-07-02 20:46:59 +0600

    added title

---
+++ app/(main)/_components/title.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface TitleProps {
+  initialData: Doc<"documents">;
+}
+
+export const Title = ({ initialData }: TitleProps) => {
+  return <div>title</div>;
+};
