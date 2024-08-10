763394459d607192eee6b506435792afe4484edc
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-08-10 19:32:31 +0600

    added icon picjer

---
+++ components/ui/icon-picker.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import EmojiPicker, { Theme } from "emoji-picker-react";
+import { useTheme } from "next-themes";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface IconPickerProps {
+  onChange: (icon: string) => void;
+}
