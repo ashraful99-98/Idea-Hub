d6c0fe2e86bdfbbc0dfb81ea92ffed049542f31c
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-08-11 22:00:18 +0600

    added textarea title

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -4,9 +4,10 @@ import { Doc } from "@/convex/_generated/dataModel";
 import { IconPicker } from "./ui/icon-picker";
 import { Button } from "@/components/ui/button";
 import { ImageIcon, Smile, X } from "lucide-react";
-import { ElementRef, useRef, useState } from "react";
+import React, { ElementRef, useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import TextareaAutosize from "react-textarea-autosize";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -40,8 +41,15 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
     });
   };
 
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
+
   return (
-    <div className=" pl-[54px] group relative">
+    <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
         <div className=" flex items-center gap-x-2 group/icon pt-6:">
           <IconPicker onChange={() => {}}>
@@ -83,9 +91,27 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
             onClick={() => {}}
           >
             <ImageIcon className=" h-4 w-4 mr-2" />
+            Add cover
           </Button>
         )}
       </div>
+      {isEditing && !preview ? (
+        <TextareaAutosize
+          ref={inputRef}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          value={value}
+          onChange={(e) => onInput(e.target.value)}
+          className=" text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className=" pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] "
+        >
+          {initialData.title}
+        </div>
+      )}
     </div>
   );
 };
