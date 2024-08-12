afa7220c3af502ba5eb1a3defa5a1253817e9db3
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-08-12 20:25:25 +0600

    added cover image model

--- components/providers/model-provider.tsx
+++ components/providers/model-provider.tsx
@@ -3,21 +3,23 @@
 import { useEffect, useState } from "react";
 import { SettingsModal } from "@/components/modals/settings-modal";
 
+import { CoverImageModel } from "@/components/modals/cover-image-model";
 
-export const ModelProvider= ()=>{
-    const [isMounted, setIsMounted] = useState(false);
+export const ModelProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(()=>{
-        setIsMounted(true);
-    },[]);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    if(!isMounted){
-        return null;
-    }
+  if (!isMounted) {
+    return null;
+  }
 
-    return(
-        <>
-          <SettingsModal/>
-        </>
-    )
-}
+  return (
+    <>
+      <SettingsModal />
+      <CoverImageModel />
+    </>
+  );
+};
--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,6 +8,7 @@ import React, { ElementRef, useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import TextareaAutosize from "react-textarea-autosize";
+import { useCoverImage } from "@/hooks/use-cover-image";
 
 interface ToolbarProps {
   initialData: Doc<"documents">;
@@ -22,6 +23,8 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
   const update = useMutation(api.documents.update);
 
   const removeIcon = useMutation(api.documents.removeIcon);
+
+  const coverImage = useCoverImage();
 
   const enableInput = () => {
     if (preview) return;
@@ -103,7 +106,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
             className=" text-muted-foreground text-xs"
             variant="outline"
             size="sm"
-            onClick={() => {}}
+            onClick={coverImage.onOpen}
           >
             <ImageIcon className=" h-4 w-4 mr-2" />
             Add cover
