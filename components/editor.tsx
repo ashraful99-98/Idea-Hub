5cab978ea68744e4151cf0817510374c0303a471
ashraful99-98 <ashrafulkamran98@gmail.com>
2024-12-25 23:26:31 +0600

    added content for note

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,88 +1,57 @@
 "use client";
 import { useTheme } from "next-themes";
-import {
-  BlockNoteEditor,
-  filterSuggestionItems,
-  PartialBlock,
-} from "@blocknote/core";
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
 import "@blocknote/core/fonts/inter.css";
-import {
-  DefaultReactSuggestionItem,
-  getDefaultReactSlashMenuItems,
-  SuggestionMenuController,
-  useCreateBlockNote,
-} from "@blocknote/react";
+import { useBlockNote, useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
-import { HiOutlineGlobeAlt } from "react-icons/hi";
-
+// import "@blocknote/core/style.css";
+import { useEditorChange } from "@blocknote/react";
+import { useCallback } from "react";
 interface EditorProps {
   onChange: (value: string) => void;
   initialContent?: string;
   editable?: boolean;
 }
 
-// Custom Slash Menu item to insert a block after the current one.
-export const insertHelloWorldItem = (editor: BlockNoteEditor) => ({
-  title: "Insert Hello World",
-  onItemClick: () => {
-    const currentBlock = editor.getTextCursorPosition().block;
-
-    const helloWorldBlock: PartialBlock = {
-      type: "paragraph",
-      content: [{ type: "text", text: "Hello World", styles: { bold: true } }],
-    };
-    editor.insertBlocks([helloWorldBlock], currentBlock, "after");
-  },
-  aliases: ["helloworld", "hw"],
-  group: "Other",
-  icon: <HiOutlineGlobeAlt size={18} />,
-  subtext: "Used to insert a block with 'Hello World' below.",
-});
-
 export const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
   const { resolvedTheme } = useTheme();
-  const getCustomSlashMenuItems = (
-    editor: BlockNoteEditor
-  ): DefaultReactSuggestionItem[] => [
-    ...getDefaultReactSlashMenuItems(editor),
-    insertHelloWorldItem(editor),
-  ];
 
-  const editor = useCreateBlockNote({
-    initialContent: [
-      // {
-      //   type: "paragraph",
-      //   content: "Welcome to this demo!",
-      // },
-      // {
-      //   type: "paragraph",
-      //   content: "Press the '/' key to open the Slash Menu",
-      // },
-      // {
-      //   type: "paragraph",
-      //   content: "Notice the new 'Insert Hello World' item - try it out!",
-      // },
-      {
-        type: "paragraph",
-      },
-    ],
+  // const editor: BlockNoteEditor = useBlockNote({
+  //   editable,
+  //   inlineContent: initialContent
+  //     ? (JSON.parse(initialContent) as PartialBlock[])
+  //     : undefined,
+
+  //   onEditorContentChange: (editor) => {
+  //     onchange(JSON.stringify(editor.topLevelBlocks, null, 2));
+  //   },
+  // });
+
+  const editor: BlockNoteEditor | null = useCreateBlockNote({
+    initialContent: initialContent
+      ? (JSON.parse(initialContent) as PartialBlock[])
+      : undefined,
   });
+  const uploadToDatabase = useCallback(() => {
+    if (onChange) {
+      setTimeout(() => {
+        onChange(JSON.stringify(editor.document));
+      }, 1000);
+    }
+  }, [editor, onChange]);
 
   return (
     <div>
       <BlockNoteView
-        editor={editor}
         theme={resolvedTheme === "dark" ? "dark" : "light"}
-        slashMenu={false}
-      >
-        <SuggestionMenuController
-          triggerCharacter={"/"}
-          getItems={async (query) =>
-            filterSuggestionItems(getCustomSlashMenuItems(editor), query)
-          }
-        />
-      </BlockNoteView>
+        editor={editor}
+        editable={editable}
+        // onChange={(editor: any) =>
+        //   onChange(JSON.stringify(editor.topLevelBlocks, null, 2))
+        // }
+        onChange={uploadToDatabase}
+      />
     </div>
   );
 };
